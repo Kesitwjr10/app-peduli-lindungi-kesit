887500031b3a551a0e5db887d3a6ba8a0152c319
Kesit Wijanarko <kesitwjr10@gmail.com>
2022-10-08 17:36:55 +0700

    mengubah index assets

--- App/assets/index.js
+++ App/assets/index.js
@@ -1,3 +1,7 @@
 import IconSplash from './img/logo-with-text.png';
+import L_Kpc from './img/logo_kpcpen.png';
+import L_Kominfo from './img/logo_kominfo.png';
+import L_Kemenkes from './img/logo_kemenkes.png';
+import L_Bumn from './img/logo_bumn.png';
 
-export {IconSplash};
+export {IconSplash, L_Kpc, L_Kominfo, L_Kemenkes, L_Bumn};
