53c68dbb9d73a05a832532a88a0e914a725642e4
Kesit Wijanarko <kesitwjr10@gmail.com>
2022-10-08 22:13:45 +0700

    mengedit route

--- App/Routes.js
+++ App/Routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {HomeScreen, SplashScreen} from './Pages';
+import {HomeScreen, SplashScreen, LoginScreen} from './Pages';
 
 const Stack = createStackNavigator();
 
 export default Router = () => {
   return (
-    <Stack.Navigator initialRouteName="SplashScreen">
+    <Stack.Navigator initialRouteName="LoginScreen">
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}}/>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
     </Stack.Navigator>
   );
 };
