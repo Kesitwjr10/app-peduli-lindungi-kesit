869af43b69d0a1a216307276fa5bc9c73d13b1e9
Kesit Wijanarko <kesitwjr10@gmail.com>
2022-10-09 14:17:22 +0700

    menambah gambar

--- App/assets/index.js
+++ App/assets/index.js
@@ -6,5 +6,39 @@ import L_Bumn from './img/logo_bumn.png';
 import L_Top from './img/logo-top.png';
 import I_Email from './img/Icon-mail.png';
 import I_Empty from './img/ic_empty.jpg';
+import I_Hand from './img/ic_hand_qr.jpg';
+import I_Vaksin from './img/ic_vaccine_and_immuniazation.jpg';
+import I_CovidResult from './img/ic_covid_test_result.jpg';
+import I_Ehac from './img/ic_ehac.jpg';
+import I_Travel from './img/ic_travel_regulation.jpg';
+import I_Telemedic from './img/ic_telemedicine.jpg';
+import I_Healt from './img/ic_healthcare_facility.jpg';
+import I_Statistic from './img/ic_statistic_covid.jpg';
+import I_FindHospital from './img/ic_find_hospital.jpg';
+import I_User from './img/user.jpeg';
+import I_Lonceng from './img/lonceng.jpeg';
+import I_Panah from './img/panah.jpeg';
 
-export {IconSplash, L_Kpc, L_Kominfo, L_Kemenkes, L_Bumn, L_Top, I_Email, I_Empty};
+
+export {
+    IconSplash, 
+    L_Kpc, 
+    L_Kominfo, 
+    L_Kemenkes, 
+    L_Bumn, 
+    L_Top, 
+    I_Email, 
+    I_Empty,
+    I_Hand,
+    I_Vaksin,
+    I_CovidResult,
+    I_Ehac,
+    I_Travel,
+    I_Telemedic,
+    I_Healt,
+    I_Statistic,
+    I_FindHospital,
+    I_User,
+    I_Lonceng,
+    I_Panah,
+};
