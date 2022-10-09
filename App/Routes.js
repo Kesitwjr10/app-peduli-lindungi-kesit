496b2d8d3826370ed0ce7ee21c3c2fcbc42bce4a
Kesit Wijanarko <kesitwjr10@gmail.com>
2022-10-09 12:42:55 +0700

    menambahkan covidtextscreen

--- App/Routes.js
+++ App/Routes.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {HomeScreen, SplashScreen, LoginScreen} from './Pages';
+import {HomeScreen, SplashScreen, LoginScreen, CovidTextScreen} from './Pages';
 
 const Stack = createStackNavigator();
 
 export default Router = () => {
   return (
-    <Stack.Navigator initialRouteName="SplashScreen">
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}}/>
-      <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
+    <Stack.Navigator initialRouteName="CovidTextScreen">
+      <Stack.Screen 
+        name="HomeScreen" 
+        component={HomeScreen} />
+      <Stack.Screen 
+        name="SplashScreen" 
+        component={SplashScreen} 
+        options={{headerShown: false}}/>
+      <Stack.Screen 
+        name="LoginScreen" 
+        component={LoginScreen} 
+        options={{headerShown: false}}/>
+      <Stack.Screen 
+        name="CovidTextScreen" 
+        component={CovidTextScreen} 
+        options={{title: "COVID -19 Test Results", headerShown: true, headerBackTitleVisible: false}}/>
     </Stack.Navigator>
   );
 };
