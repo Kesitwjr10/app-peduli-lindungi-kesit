461ab435b76a58cf670fc4644c3cf47a8e420d91
Kesit Wijanarko <kesitwjr10@gmail.com>
2022-10-09 14:16:52 +0700

    mengedit

--- App/Routes.js
+++ App/Routes.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {HomeScreen, SplashScreen, LoginScreen, CovidTextScreen} from './Pages';
 
 const Stack = createStackNavigator();
 
 export default Router = () => {
   return (
-    <Stack.Navigator initialRouteName="CovidTextScreen">
+    <Stack.Navigator initialRouteName="HomeScreen">
       <Stack.Screen 
         name="HomeScreen" 
-        component={HomeScreen} />
+        component={HomeScreen}
+        options={{headerShown: false}} />
       <Stack.Screen 
         name="SplashScreen" 
         component={SplashScreen} 
-        options={{headerShown: false}}/>
+        options={{headerShown: false}} />
       <Stack.Screen 
         name="LoginScreen" 
         component={LoginScreen} 
-        options={{headerShown: false}}/>
+        options={{headerShown: false}} />
       <Stack.Screen 
         name="CovidTextScreen" 
         component={CovidTextScreen} 
-        options={{title: "COVID -19 Test Results", headerShown: true, headerBackTitleVisible: false}}/>
+        options={{title: "COVID -19 Test Results", 
+                  headerShown: true, 
+                  headerBackTitleVisible: false}} />
     </Stack.Navigator>
   );
 };
