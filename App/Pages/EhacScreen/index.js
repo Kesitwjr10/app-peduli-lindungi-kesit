ec6b34c15ef978107f5b0b8fdd7d18cee812083f
Kesit Wijanarko <kesitwjr10@gmail.com>
2022-10-10 17:06:50 +0700

    edit ehac screen

--- App/Pages/EhacScreen/index.js
+++ App/Pages/EhacScreen/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import {View, Text, StyleSheet, Image, Dimensions,} from "react-native";
-import {bgtopehac, myehac, crtehac} from "../../assets";
+import * as React from 'react';
+import {View, Text, StyleSheet, Image, Dimensions, StatusBar} from "react-native";
+import {L_Backgrond, L_CreateEhc, L_MyEhc} from "../../assets";
 
 const windowHeight = Dimensions.get("window").height;
 const {width, height} = Dimensions.get("window");
@@ -8,24 +8,31 @@ const {width, height} = Dimensions.get("window");
 export default EhacScreen = ({ navigation }) => {
     return (
         <View style={styles.bgApp}>
+            <StatusBar
+                barStyle="light-content"
+                animated={true}
+                backgroundColor='transparent' translucent />
+
             <View>
-                <Image source={bgtopehac} style={styles.gambartop} />
+                <Image source={L_Backgrond} style={styles.gambartop} />
             </View>
 
             <View style={styles.item}>
                 <View style={{flexDirection: 'row'}}>
-                    <Image source={crtehac} style={styles.gambar1}/>
+                    <Image source={L_MyEhc} style={styles.gambar1}/>
                         <View style={{flexDirection: 'column'}}>
                             <Text style={styles.teks1}>Create e-HAC</Text>
-                            <Text style={styles.teks11}>Electronic Health Alert Card</Text>
+                            <Text style={styles.teks1a}>Electronic Health Alert Card</Text>
                         </View>
                 </View>
-                    <View style={{borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth,width:220,marginStart:90,}} />
+
+                <View style={{borderBottomColor: 'black', borderBottomWidth: StyleSheet.hairlineWidth,width:206,marginStart:80,}} />
+                
                 <View style={{flexDirection: 'row'}}>
-                    <Image source={myehac} style={styles.gambar2}/>
+                    <Image source={L_CreateEhc} style={styles.gambar2}/>
                         <View style={{flexDirection: 'column'}}>
                             <Text style={styles.teks2}>My e-HAC</Text>
-                            <Text style={styles.teks22}>Check your history eHAC here</Text>
+                            <Text style={styles.teks2a}>Check your history eHAC here</Text>
                         </View>
                 </View>
             </View>
@@ -40,14 +47,14 @@ const styles = StyleSheet.create({
         },
         item: {
             backgroundColor: "white",
-            padding: 15,
-            borderRadius: 15,
+            padding: 20,
+            borderRadius: 30,
             flexDirection: 'column',
             justifyContent: 'space-between',
-            height: 220,
-            marginTop: windowHeight / 23,
-            marginLeft: 17,
-            marginRight: 17,
+            height: 215,
+            marginTop: windowHeight / 35,
+            marginLeft: 27,
+            marginRight: 27,
             shadowColor: "black",
             shadowOffset: {
             width: 0,
@@ -57,44 +64,44 @@ const styles = StyleSheet.create({
             elevation: 24,
         },
         gambartop: {
-            height: 230,
+            height: 200,
             width: width,
             
         },
         gambar1: {
             borderRadius: 20,
-            height:80,
-            width:80,
+            height: 70,
+            width: 70,
             resizeMode: "contain"
         },
         gambar2: {
             borderRadius: 20,
-            height:80,
-            width:80,
+            height: 70,
+            width: 70,
             resizeMode: 'contain'
         },
         teks1: {
             fontWeight: 'bold',
-            fontSize: 18,
+            fontSize: 16,
             top: 5,
             marginLeft: 10
         },
-        teks11: {
-            fontSize: 16,
+        teks1a: {
+            fontSize: 14,
             top: 5,
             marginLeft: 10,
-            color: '#8A8A8A'
+            color:"#8A8A8A"
         },
         teks2: {
             fontWeight: 'bold',
-            fontSize: 18,
+            fontSize: 16,
             top: 5,
             marginLeft: 10
         },
-        teks22: {
-            fontSize: 16,
+        teks2a: {
+            fontSize: 14,
             top: 5,
             marginLeft: 10,
-            color: '#8A8A8A'
+            color:"#8A8A8A"
         },
       });
